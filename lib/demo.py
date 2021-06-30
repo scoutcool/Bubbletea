2b8489d83de44fc3a8ef76fac6b47efd1a7412f1
CJ <cxjiang@gmail.com>
2021-06-30 17:12:56 -0400

    Add demo files

---
+++ lib/demo.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+import os
+
+st.set_page_config(page_title='Earlgrey Demos', page_icon=':coin:', layout='wide', initial_sidebar_state='collapsed')
+st.sidebar.title(":hot_pepper: Demos")
+
+with st.beta_expander("About Earlgrey"):
+    with open('README.md', 'r') as file:
+        intro = file.read()
+        st.markdown(intro)
+
+display, editor = st.beta_columns((1, 1))
+
+
+selected_demo = None
+with st.sidebar:
+    files = [filename for filename in os.listdir('./lib') if filename.startswith("demo_")]
+    selected_demo = st.selectbox('🌟 Pick one', sorted(files))
+
+code_input = """st.header("Earlgrey Demos")"""
+with open(f'./lib/{selected_demo}', 'r') as file:
+    code_input = file.read()
+
+with editor:
+    st.markdown(f'```{code_input}')
+
+with display:
+    exec(code_input)
