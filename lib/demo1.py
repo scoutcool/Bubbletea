653d12e072a8d17523b42daa19ea251da1bcc23a
Kuan Huang <huangkuan@gmail.com>
2021-06-30 19:24:44 -0400

    demo1

---
+++ lib/demo1.py
@@ -0,0 +1,27 @@
+from earlgrey.thegraph import thegraph_loader as gl
+import pandas as pd
+from earlgrey.charts import line as l
+
+url_aave_subgraph = "https://api.thegraph.com/subgraphs/name/aave/protocol-v2"
+query_aave = """
+{
+    deposits(
+        where:{timestamp_gt:1609459200, timestamp_lt:1610236800}
+        orderBy: timestamp
+        orderDirection: asc
+        bypassPagination: true
+    ) {
+        amount
+        timestamp
+    }
+}
+"""
+
+data = gl.load_subgraph(url_aave_subgraph, query_aave)
+data = pd.json_normalize(data["data"]["deposits"])
+data["timestamp"] = data["timestamp"].apply(lambda x: pd.to_datetime(x, unit="s")) # will be removed once chart lib is updated
+
+l.plot(data, 
+    x={"title":"Time", "field":"timestamp"},
+    ys = [{"title":"Amount", "field":"amount"}]
+    )
