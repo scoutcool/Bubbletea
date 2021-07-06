2f9b5b86d65823791d565feb462852e1cf4cbddc
Kuan Huang <huangkuan@gmail.com>
2021-07-05 22:47:07 -0400

    demo1 removed conversion

--- lib/demo1.py
+++ lib/demo1.py
@@ -2,11 +2,14 @@ from earlgrey.thegraph import thegraph_loader as gl
 import pandas as pd
 from earlgrey.charts import line as l
 
+start_timestamp = 1609459200
+end_timestamp = 1610236800
+
 url_aave_subgraph = "https://api.thegraph.com/subgraphs/name/aave/protocol-v2"
 query_aave = """
 {
     deposits(
-        where:{timestamp_gt:1609459200, timestamp_lt:1610236800}
+        where:{timestamp_gt:%s, timestamp_lt:%s}
         orderBy: timestamp
         orderDirection: asc
         bypassPagination: true
@@ -15,14 +18,13 @@ query_aave = """
         timestamp
     }
 }
-"""
+""" % (
+    start_timestamp,
+    end_timestamp,
+)
 
 data = gl.load_subgraph(url_aave_subgraph, query_aave)
 data = pd.json_normalize(data["data"]["deposits"])
-data["timestamp"] = data["timestamp"].apply(
-    lambda x: pd.to_datetime(x, unit="s")
-)  # will be removed once chart lib is updated
-
 l.plot(
     data,
     x={"title": "Time", "field": "timestamp"},
