934544582965d24412fa7e9d607037da6ae36080
Leejay <leejayxia@gmail.com>
2021-07-06 18:13:16 -0400

    Add datetime inference from strings
    Fix demo_2 aggregation
    Fix https://app.asana.com/0/1200385967382973/1200568243786905

--- lib/demo_2.py
+++ lib/demo_2.py
@@ -8,10 +8,12 @@ import os
 start_timestamp = 1609459200
 end_timestamp = 1610236800
 
-CP_API_TOKEN = "163b4f02ba446862200ecf7a64c3359b6d6bcf9d417aa27a0b5b29c9f9e619be"#os.environ.get("cp_api_token")
-pricing = cp.load_historical_data("AAVE", "USD", start_timestamp, end_timestamp, CP_API_TOKEN, 2000)
+CP_API_TOKEN = "163b4f02ba446862200ecf7a64c3359b6d6bcf9d417aa27a0b5b29c9f9e619be"  # os.environ.get("cp_api_token")
+pricing = cp.load_historical_data(
+    "AAVE", "USD", start_timestamp, end_timestamp, CP_API_TOKEN, 2000
+)
 pricing_df = pd.json_normalize(pricing)
-print(pricing_df[["time","close"]])   
+print(pricing_df[["time", "close"]])
 
 
 url_aave_subgraph = "https://api.thegraph.com/subgraphs/name/aave/protocol-v2"
@@ -41,17 +43,20 @@ data_hourly_df = ts.aggregate_timeseries(
     columns=[
         ts.ColumnConfig(
             name="amount",
+            type="float",
             aggregate_method=ts.AggregateMethod.SUM,
             na_fill_value=0.0,
         )
-    ]
+    ],
 )
 data_hourly_df = data_hourly_df.reset_index()
 print(data_hourly_df)
 result = pd.concat([pricing_df, data_hourly_df], axis=1)
 print(result)
 
-#l.plot(data_hourly, 
-#    x={"title":"Time", "field":"timestamp"},
-#    yLeft = [{"title":"Price", "field":"amount"}]
-#    )
+
+l.plot(
+    data_hourly_df,
+    x={"title": "Time", "field": "timestamp"},
+    yLeft=[{"title": "Price", "field": "amount"}],
+)
